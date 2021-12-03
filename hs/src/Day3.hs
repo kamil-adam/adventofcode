b9d803dcb9292ec73c9818c7c38e3c511f9d7d00
Kamil Adam <kamil.adam.zabinski@gmail.com>
2021-12-03 06:53:28 +0100

    Day3

--- hs/src/Day3.hs
+++ hs/src/Day3.hs
@@ -1,15 +1,55 @@
-module Day2 where
+module Day3 where
+
+import           Relude.Unsafe
 
 day3 :: IO ()
 day3 = do
   content <- readFileText "input/input3"
-  putTextLn $ show $ aaa content
+  putTextLn $ show $ run2 content
 
 run :: Text -> [Bool]
-run s = ccc <$> transpose $ aaa <$> lines s
+run s = ccc <$> transpose2 (aaa <$> lines s)
 
-count :: [Text] -> Int
-count l = length l
+run2 :: Text -> [Bool]
+run2 s = aaa3 $ (aaa <$> lines s)
+
+-- [False,True,False,False,True,False,False,False,True,False,False,False]
+--010010001000 = 1160
+
+
+aaa3 :: [[Bool]] -> [Bool]
+aaa3 l
+  | 0 < length l = index2 "aaa3" (aaa4 0 l) 0
+  | otherwise = error "empty list"
+
+aaa4 :: Int -> [[Bool]] -> [[Bool]]
+aaa4 i l
+  | 1 == (length l) = l
+  | i < (length $ index2 "aaa4" l 0) = aaa4 (i + 1) (filterList i l)
+  | otherwise = l
+
+filterList :: Int -> [[Bool]] -> [[Bool]]
+filterList i l
+  | 1 == (length l) = l
+  | i < (length $ index2 "filterList" l 0) = filter (\al -> (index2 "filterList" al i) == (notCCC $ bbb2 i <$> l)) l
+  | otherwise = error $ show i <> show l
+
+
+
+bbb2 :: Int -> [Bool] -> Bool
+bbb2 i l = l !! i
+
+
+index2:: (Show a) => Text -> [a] -> Int -> a
+index2 s l i
+  | 0 < length l = l !! i
+  | otherwise = error $ show s <> " " <> show i <> " " <> show l
+
+
+
+--111001011100 = 3676
+--000110100011 = 419
+--1540244
 
 aaa :: Text -> [Bool]
 aaa l = bbb <$> toString l
@@ -19,8 +59,11 @@ bbb '0' = False
 bbb '1' = True
 bbb  c  = error $ show c
 
+notCCC :: [Bool] -> Bool
+notCCC = not . ccc
+
 ccc :: [Bool] -> Bool
-ccc l = 1/2 <= (length $ filter id l)/(length l)
+ccc l = (2 * length (filter id l)) <= length l
 
 commend :: [Text] -> (Int , Int)
 commend ["forward" , a] = (readInt a , 0)
@@ -47,14 +90,14 @@ productPair (a , b) = a * b
 
 ----
 
-transpose :: [[a]] -> [[a]]
-transpose = getZipList . traverse ZipList
+transpose2 :: [[a]] -> [[a]]
+transpose2 = getZipList . traverse ZipList
 
-newtype ZipList a = ZipList { getZipList :: [a] }
-
-instance Applicative ZipList where
-    pure x = ZipList (repeat x)
-    ZipList fs <*> ZipList xs = ZipList (zipWith ($) fs xs)
+--newtype ZipList a = ZipList { getZipList :: [a] }
+--
+--instance Applicative ZipList where
+--    pure x = ZipList (repeat x)
+--    ZipList fs <*> ZipList xs = ZipList (zipWith ($) fs xs)
 
 
 ------
