50227b1bfa212b03e8b7ecfd74e1f58844a599e0
Kamil Adam <kamil.adam.zabinski@gmail.com>
2021-12-08 18:13:03 +0100

    Day8

--- hs/src/Day8.hs
+++ hs/src/Day8.hs
@@ -1,51 +1,77 @@
---{-# LANGUAGE BangPatterns, CPP, MagicHash, Rank2Types, UnboxedTuples, TypeFamilies #-}
---{-# LANGUAGE TemplateHaskellQuotes #-}
---{-# LANGUAGE Trustworthy #-}
-
 module Day8 where
 
---import qualified          Relude.Unsafe as Unsafe
-
---import           Data.List.Split
-
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
-import qualified Data.List as L
+--import qualified Data.List as L
 import qualified Data.Text as T
 
 day8 :: IO ()
 day8 = do
 --  content <- readFileText "input/i8"
+--  content <- readFileText "input/in8"
   content <- readFileText "input/input8"
   putTextLn $ "day 8 " <> (show $ run content)
 
-run :: Text -> (Int , Int)
-run t = minForTuple $ (\i -> aaa i positions) <$> [(L.minimum positions) .. (L.maximum positions)]
---run t = (\i -> aaa i positions) <$> [(L.minimum positions) .. (L.maximum positions)]
-  where positions = readInt <$> T.splitOn "," t
+type Return = Int
 
-minForTuple :: [(Int , Int)] -> (Int , Int)
-minForTuple l = L.foldl1 bbb l
+run :: Text -> Return
+run t = sum $ aaa2 <$> T.splitOn "|" <$> lines t
 
-bbb :: (Int , Int) -> (Int , Int) -> (Int , Int)
-bbb (k1 , v1) (k2 , v2)
-  | v1 <= v2  = (k1 , v1)
-  | otherwise = (k2 , v2)
+--aaa :: [Text] -> Return
+--aaa [ p , a ]  = mull $ checkText m <$> words a where m = findSimple p
+--aaa  a         = error $ show a
 
-aaa :: Int -> [Int] -> (Int , Int)
-aaa i l = (i , sum $ normalize2 i l)
+aaa2 :: [Text] -> Return
+aaa2 [ p , a ]  = mull $ (decode m <$> words a) where m = (findSimple $ words p)
+aaa2  a         = error $ show a
 
-normalize2 :: Int -> [Int] -> [Int]
-normalize2 i l = (\ i' -> cal $ abs $ i' - i) <$> l
+mull :: [Int] -> Int
+mull [a1, a2, a3, a4] = a1 * 1000 + a2 * 100 + a3 * 10 + a4
+mull a = error $ show a
 
-cal :: Int -> Int
-cal i = go 0 i where
-  go s 0 = s
-  go s i' = go (s + i') (i' - 1)
+decode :: Map Int Text -> Text -> Int
+decode m t
+  | (T.length t == 2) = 1
+  | (T.length t == 3) = 7
+  | (T.length t == 4) = 4
+  | (T.length t == 7) = 8
+  | (T.length t == 6) && (contestText t (m Map.! 7)) && (contestText t (m Map.! 4)) = 9
+  | (T.length t == 6) && (contestText t (m Map.! 7))  = 0
+  | (T.length t == 6) = 6
+  | (T.length t == 5) && (contestText t (m Map.! 1)) = 3
+  | (T.length t == 5) && (contestText' t (m Map.! 4)) == 3 = 5
+  | (T.length t == 5) && (contestText' t (m Map.! 4)) == 2 = 2
+  | otherwise = error $ show m <> " " <> show t <> " " <> (show $ T.length t)
 
--- 1 -> 1
--- 2 -> 3
--- 3 -> 6
+contestText :: Text -> Text -> Bool
+contestText t2 t1 = Set.isSubsetOf (textToCharSet t1) (textToCharSet t2)
+
+contestText' :: Text -> Text -> Int
+contestText' t2 t1 = Set.size $ Set.intersection (textToCharSet t1) (textToCharSet t2)
+
+
+textToCharSet :: Text -> Set Char
+textToCharSet t = fromList $ toString t
+
+findSimple :: [Text] -> Map Int Text
+findSimple t = fromList [findPattern 1 2 t , findPattern 7 3 t , findPattern 4 4 t , findPattern 8 7 t]
+
+findPattern :: Int -> Int -> [Text] -> (Int, Text)
+findPattern i s l = (i , getMaybe $ find (\e -> T.length e == s) l)
+
+getMaybe :: Maybe a -> a
+getMaybe (Just a) = a
+getMaybe Nothing  = error "e"
+
+
+--ccc
+--ccc 2 = 1
+--ccc 3 = 7
+--ccc 4 = 4
+--ccc 7 = 8
+
+--ddd
 
 normalize :: Int -> [Int] -> [Int]
 normalize i l = (\ i' -> abs $ i' - i) <$> l
