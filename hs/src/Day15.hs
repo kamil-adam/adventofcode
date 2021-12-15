c54397f24a3b73877ae95b8bc64d53d35789e233
Kamil Adam <kamil.adam.zabinski@gmail.com>
2021-12-15 06:25:31 +0100

    Day15

--- hs/src/Day15.hs
+++ hs/src/Day15.hs
@@ -5,101 +5,44 @@ import qualified Data.Set        as Set
 
 import qualified Relude.Unsafe   as Unsafe
 
---import qualified Data.List     as L
+import qualified Data.List     as L
 import qualified Data.Text       as T
 
-import qualified Data.List.Extra as L
+--import qualified Data.List.Extra as L
 
 day15:: IO ()
 day15 = do
---  t <- readFileText "input/i15" -- NNCB
+  t <- readFileText "input/i15" -- NNCB
 --  t <- readFileText "input/i15" --17
 --  t <- readFileText "input/in15" --19 --103
-  t <- readFileText "input/input15" -- SHPPPVOFPBFCHHBKBNCV
---  putTextLn $ "day 15 \n" <> (show $ (run2 "NNCB" t))
---  putTextLn $ "day 15 \n" <> (show $ (run2 "SHPPPVOFPBFCHHBKBNCV" t))
-  putTextLn $ "day 15 \n" <> (show $ (run2 "SHPPPVOFPBFCHHBKBNCV" t))
+--  t <- readFileText "input/input15" -- SHPPPVOFPBFCHHBKBNCV
+  putTextLn $ "day 15 \n" <> (show $ (run1 t))
 
 type Return = Int
---type Return = StateMap
---type Return = [(CharPair, Int)]
 --type Return = [Int]
---type Return = [(Char, Int)]
---type Return = Map Char Int
+--type Return = Board
 
-run2 :: Text -> Text -> Return
---run2 start t = count $ Map.toList $ step2 40 (buildStateMap $ toString start) (buildMatchMap t)
-run2 start t = count' $ sort $ snd <$> correctLast <$> Map.toList (Map.map sum $ buildMap $ ((\ ((c1 , _), i ) -> (c1, i)) <$> (Map.toList $ step2 40 (buildStateMap $ toString start) (buildMatchMap t))))
+run1 :: Text -> Return
+run1 t = L.minimum $ start $ map readIntFromChar <$> toString <$> lines t
 
-correctLast :: (Char , Int) -> (Char , Int)
-correctLast ('V' , i) = ('V' , i +1)
-correctLast a         = a
+start :: Board -> [Int]
+start b = step 0 (0, 0) end b where end = ((length b) - 1, (length (Unsafe.head b)) - 1)
 
+step :: Int -> Point -> Point -> Board -> [Int]
+step s p@(i1 , i2) pe@(e1 , e2) b
+  | i1 < e1 && i2 < e2 = (step s' (i1 + 1 , i2) pe b) <> (step s' (i1 , i2 + 1) pe b)
+  | i1 < e1            = (step s' (i1 + 1 , i2) pe b)
+  | i2 < e2            = (step s' (i1 , i2 + 1) pe b)
+  | otherwise          = [s]
+    where
+      s' = s + v
+      v = valuePoint b p
 
-step2 :: Int -> StateMap -> MatchMap -> StateMap
-step2 0 s _        = s
-step2 i s matchMap = step2 (i - 1)  s' matchMap
-  where s' = nextState2 matchMap s
-
-nextState2 :: MatchMap -> StateMap -> StateMap
-nextState2 matchMap s = Map.map sum (buildMap $ concat (nextState2' matchMap <$> (Map.toList s)))
-
-nextState2' :: MatchMap -> CharPairWithCount -> [CharPairWithCount]
-nextState2' matchMap ((c1 , c2) , i) = [((c1 , c), i) , ((c , c2), i)]
- where c = match2chars matchMap c1 c2
-
-buildStateMap :: String -> StateMap
-buildStateMap s = Map.fromList $ sortGroupAndCount $ buildStateMap' s
-
-buildStateMap' :: String -> [CharPair]
-buildStateMap' []            = []
-buildStateMap' [_]           = []
-buildStateMap' (c1 : c2 : s) = (c1 , c2) : (buildStateMap' (c2 : s))
+valuePoint :: Board -> Point -> Int
+valuePoint b (i1 , i2) = (b Unsafe.!! i1) Unsafe.!! i2
 
 
-type StateMap = Map CharPair Int
 
-type CharPairWithCount = (CharPair , Int)
-
-type CharPair = (Char , Char)
-
-run1 :: Text -> Text -> Int
-run1 start t = count $ step 10 (toString start) (buildMatchMap t)
-
-count :: (Ord a ) => [a] -> Int
-count l = count' $ sort (length <$> sortAndGroup l)
-
-count' :: [Int] -> Int
-count' l = (Unsafe.last l) - (Unsafe.head l)
-
-step :: Int -> String -> MatchMap -> String
-step 0 s _        = s
-step i s matchMap = step (i - 1) s' matchMap
-  where s' = nextState matchMap s
-
-nextState :: MatchMap -> String -> String
-nextState _                 [] = []
-nextState _                [c] = [c]
-nextState matchMap (c1: c2: s) = c1 : (match2chars matchMap c1 c2) : (nextState matchMap (c2:s))
-
-buildMatchMap :: Text -> MatchMap
-buildMatchMap t = buildMap <$> (buildMap $ buildMatchMap' <$> T.splitOn " -> " <$> lines t)
-
-buildMap :: (Ord k , Ord v) => [(k , v)] -> Map k [v]
-buildMap l = Map.fromList (aaa <$> (L.groupOn (\(k , _) -> k) $ sort l))
-
-aaa :: [(k , v)] -> (k , [v])
-aaa []              = error "aaa"
-aaa l@((k , _) : _) = (k, (snd <$> l))
-
-buildMatchMap' :: [Text] -> (Char , (Char , Char))
-buildMatchMap' [t1 , t2] = (T.index t1 0 , (T.index t1 1 , T.index t2 0))
-buildMatchMap' other     = error $ show other
-
-type MatchMap = Map Char (Map Char [Char])
-
-match2chars :: MatchMap -> Char -> Char -> Char
-match2chars matchMap c1 c2 = ((matchMap Map.! c1) Map.! c2) Unsafe.!! 0
 
 sortGroupAndCount :: (Ord a) => [a] -> [(a , Int)]
 sortGroupAndCount l = countDuplicates <$> sortAndGroup l
